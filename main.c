90dde99a24d2e03b2675ebeeda3d730998a3c9fd
dxdxffg <dxdxffgg99@gmail.com>
2026-04-15 00:38:53 +0900

    ADD: refactor

--- main.c
+++ main.c
@@ -6,16 +6,16 @@ int main(void) {
     if (cpu_init(&cpu, 1024)) { return 1; }
 
     const instr_t program[] = {
-        { MOV, 1, 0, 0, 0, 100000000 },
-        { MOV, 1, 1, 0, 0, 0 },
+        { MOV, 1, r(0), 0, 0, 100000000 },
+        { MOV, 1, r(1), 0, 0, 0 },
 
-        { ADD, 0, 1, 1, 0, 0 },
-        { XOR, 1, 1, 1, 0, 1234567 },
-        { ADD, 1, 0, 0, 0, -1 },
-        { CMP, 1, 0, 0, 0, 0 },
+        { ADD, 0, r(1), r(0), 0, 0 },
+        { XOR, 1, r(1), r(1), 0, 1234567 },
+        { ADD, 1, r(0), 0, 0, -1 },
+        { CMP, 1, r(0), 0, 0, 0 },
         { JG, 1, 0, 0, 0, 26 },
 
-        { EOP, 0, 0, 0, 0, 0 },
+        { EOP, 0, 0, 0, 0, 0 },,,
     };
 
     load_program(&cpu, program, sizeof(program) / sizeof(instr_t));
